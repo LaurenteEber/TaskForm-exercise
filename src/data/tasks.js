f3fd7dfb8dc8afc436d4f0c583b21ba6e84e248f
Eber Laurente <laurente.eber@outlook.com>
2022-09-27 07:01:04 -0500

    deploy commint

---
+++ src/data/tasks.js
@@ -0,0 +1,32 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primera tarea",
+    description: "mi primera tarea",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "mi segunda tarea",
+  },
+  {
+    id: 2,
+    title: "mi tercera tarea",
+    description: "mi tercera tarea",
+  },
+  {
+    id: 3,
+    title: "mi cuarta tarea",
+    description: "mi cuarta tarea",
+  },
+  {
+    id: 4,
+    title: "mi quinta tarea",
+    description: "mi quinta tarea",
+  },
+  {
+    id: 5,
+    title: "mi sexta tarea",
+    description: "mi sexta tarea",
+  },
+]
